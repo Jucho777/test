779a3721affc95be128546965dbe1e405a17ca09
Jucho777 <85242163+Jucho777@users.noreply.github.com>
2021-06-14 10:25:13 -0600

    Add files via upload

---
+++ CoderbyteTest/src/main/java/com/example/coderbyte/test/CoderbyteTestApplication.java
@@ -0,0 +1,13 @@
+package com.example.coderbyte.test;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CoderbyteTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CoderbyteTestApplication.class, args);
+	}
+
+}
---
+++ CoderbyteTest/src/test/java/com/example/coderbyte/test/CoderbyteTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.coderbyte.test;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CoderbyteTestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
